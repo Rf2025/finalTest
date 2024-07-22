ae39f6267dfc8824902e45712e60ad433e619d38
Rf2025 <165812829+Rf2025@users.noreply.github.com>
2024-07-22 19:55:14 -0400

    changed host header permissions

--- server/backEnd.js
+++ server/backEnd.js
@@ -1,13 +1,77 @@
+// const express = require('express');
+// const mysql = require('mysql2');
+// require('dotenv').config();
+
+
+// const app = express();
+// const PORT = process.env.PORT || 8200;
+
+
+// // create connection to dd
+// const connection = mysql.createConnection({
+//     host: process.env.DB_HOST,
+//     user: process.env.DB_USER,
+//     port: process.env.DB_PORT,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_DATABASE
+// });
+
+// connection.connect((err) => {
+//     if (err) {
+//         console.error('Error connectting to database', err);
+//     } else {
+//         console.log('Connected to database');
+//     }
+// });
+
+
+// // this route gets the products from the database, we also use this route to fetch products from the front end
+// app.get('/product/items', (req, res) => {
+//     connection.query('SELECT product_name, product_description, product_image, product_price,setup_type FROM products', (err, results) => {
+//         if (err) {
+//             console.error('Error fetching products', err);
+//             res.status(500).json({  'Failed to fetch products':err});
+//         } else {
+//             console.log('PRODUCTS::', results);
+//             res.json(results);
+//         }
+//     });
+// });
+
+
+
+
+
+// // this route is used to add user information from our form to the database
+// app.post('/form', (req, res) => {
+//     const data = req.body;
+//     console.log('User data::', data);
+//     res.send('received user data');
+// });
+
+
+// // tells express to server our static builds folder
+// app.use(express.static('build'));
+
+
+
+
+// app.listen(PORT, () => {
+//     console.log(`Server is running on http://localhost:${PORT}`);
+// });
+
+
+
+
+
 const express = require('express');
 const mysql = require('mysql2');
 require('dotenv').config();
 
-
 const app = express();
 const PORT = process.env.PORT || 8200;
 
-
-// create connection to dd
+// Create connection to database
 const connection = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
@@ -18,19 +82,28 @@ const connection = mysql.createConnection({
 
 connection.connect((err) => {
     if (err) {
-        console.error('Error connectting to database', err);
+        console.error('Error connecting to database', err);
     } else {
         console.log('Connected to database');
     }
 });
 
+// permissions to set header to avoid invalid host header error
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    res.setHeader('Host', 'https://everythingsi-c2e52ca97ff8.herokuapp.com/'); 
+    next();
+});
 
-// this route gets the products from the database, we also use this route to fetch products from the front end
+// Route to fetch products from the database
 app.get('/product/items', (req, res) => {
-    connection.query('SELECT product_name, product_description, product_image, product_price,setup_type FROM products', (err, results) => {
+    connection.query('SELECT product_name, product_description, product_image, product_price, setup_type FROM products', (err, results) => {
         if (err) {
             console.error('Error fetching products', err);
-            res.status(500).json({  'Failed to fetch products':err});
+            res.status(500).json({ error: 'Failed to fetch products' });
         } else {
             console.log('PRODUCTS::', results);
             res.json(results);
@@ -39,28 +112,16 @@ app.get('/product/items', (req, res) => {
 });
 
 
-
-
-
-// this route is used to add user information from our form to the database
 app.post('/form', (req, res) => {
     const data = req.body;
-    console.log('User data::', data);
-    res.send('received user data');
+    console.log('User data:', data);
+    res.send('Received user data');
 });
 
 
-// tells express to server our static builds folder
 app.use(express.static('build'));
 
 
-
-
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
-
-
-
-
-

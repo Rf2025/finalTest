190387320353fcbb85007b3ab57766aabad172f6
Rf2025 <165812829+Rf2025@users.noreply.github.com>
2024-07-23 13:33:20 -0400

    changed routes in nav

--- src/Navbar.js
+++ src/Navbar.js
@@ -6,21 +6,20 @@ import navCSS from './Nav.module.css'
 import logo from './silogo.jpeg'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBars } from '@fortawesome/free-solid-svg-icons'
-import { Link } from 'react-router-dom';
 
 
 
 function Navbar(){
     return(
 <header>
-  <nav className={navCSS.navbar}>
+<nav className={navCSS.navbar}>
     <div className={navCSS.logoContainer} alt='everything-si-logo'>
-    <Link to='/'><img src={logo} alt="everything-si logo" className={navCSS.logoImg}/></Link>
+  <a href="/home">  <img src={logo}  className={navCSS.logoImg}/></a>
     </div>
     <ul>
-      <Link to='/'><li>Home</li></Link>
-      <Link to='/Products'><li>Products</li></Link>
-      <Link to='/Contact'><li>Contact</li></Link>
+<li><a className={navCSS.navLinks} href="/">Home</a></li>
+<li><a className={navCSS.navLinks} href="/products">Products</a></li>
+<li><a className={navCSS.navLinks} href="/contact">Contact</a></li>
     </ul>
 <button className={navCSS.menuBtn}><FontAwesomeIcon icon={faBars}   alt="menu-icon"/></button>
   </nav>

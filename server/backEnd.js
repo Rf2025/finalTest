e0c424cc2188f4c648b5eb2a77363e8a34596953
Rf2025 <165812829+Rf2025@users.noreply.github.com>
2024-07-22 20:00:41 -0400

    changed host header permissions

--- server/backEnd.js
+++ server/backEnd.js
@@ -94,9 +94,9 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Credentials', true);
-    res.setHeader('Host', 'https://everythingsi-c2e52ca97ff8.herokuapp.com/'); 
     next();
 });
+
 
 // Route to fetch products from the database
 app.get('/product/items', (req, res) => {

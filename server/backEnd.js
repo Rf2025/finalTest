263100109f4a007673d66fe0e491027f8d18d215
Rf2025 <165812829+Rf2025@users.noreply.github.com>
2024-07-23 13:21:43 -0400

    changed server routes

--- server/backEnd.js
+++ server/backEnd.js
@@ -114,7 +114,7 @@ app.get('/product/items', (req, res) => {
 });
 
 app.get('*',(req,res)=>{
-    res.sendFile(path.join(__dirname, "../build"))
+    res.sendFile(path.join(__dirname, "../build/index.html"))
 });
 
 

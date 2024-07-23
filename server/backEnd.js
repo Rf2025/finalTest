774e2774e76c58224d4dcb75bf366b52152005a5
Rf2025 <165812829+Rf2025@users.noreply.github.com>
2024-07-23 09:28:04 -0400

    changed structure

--- server/backEnd.js
+++ server/backEnd.js
@@ -96,7 +96,7 @@ connection.connect((err) => {
 });
 
 // Route to fetch products from the database
-app.get('/', (req, res) => {
+app.get('/product/items', (req, res) => {
     connection.query('SELECT product_name, product_description, product_image, product_price, setup_type FROM products', (err, results) => {
         if (err) {
             console.error('Error fetching products', err);

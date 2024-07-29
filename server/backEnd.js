c623a6639143df79ee111df9eded335ee8b01430
Rf2025 <165812829+Rf2025@users.noreply.github.com>
2024-07-29 17:22:25 -0400

    restructured files

--- server/backEnd.js
+++ server/backEnd.js
@@ -1,70 +1,3 @@
-// const express = require('express');
-// const mysql = require('mysql2');
-// require('dotenv').config();
-
-
-// const app = express();
-// const PORT = process.env.PORT || 8200;
-
-
-// // create connection to dd
-// const connection = mysql.createConnection({
-//     host: process.env.DB_HOST,
-//     user: process.env.DB_USER,
-//     port: process.env.DB_PORT,
-//     password: process.env.DB_PASSWORD,
-//     database: process.env.DB_DATABASE
-// });
-
-// connection.connect((err) => {
-//     if (err) {
-//         console.error('Error connectting to database', err);
-//     } else {
-//         console.log('Connected to database');
-//     }
-// });
-
-
-// // this route gets the products from the database, we also use this route to fetch products from the front end
-// app.get('/product/items', (req, res) => {
-//     connection.query('SELECT product_name, product_description, product_image, product_price,setup_type FROM products', (err, results) => {
-//         if (err) {
-//             console.error('Error fetching products', err);
-//             res.status(500).json({  'Failed to fetch products':err});
-//         } else {
-//             console.log('PRODUCTS::', results);
-//             res.json(results);
-//         }
-//     });
-// });
-
-
-
-
-
-// // this route is used to add user information from our form to the database
-// app.post('/form', (req, res) => {
-//     const data = req.body;
-//     console.log('User data::', data);
-//     res.send('received user data');
-// });
-
-
-// // tells express to server our static builds folder
-// app.use(express.static('build'));
-
-
-
-
-// app.listen(PORT, () => {
-//     console.log(`Server is running on http://localhost:${PORT}`);
-// });
-
-
-
-
-
-
 const express = require('express');
 const mysql = require('mysql2');
 require('dotenv').config();
@@ -74,6 +7,8 @@ const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 8200;
 
+
+// 
 const courseOption ={
     origin:['http://localhost:8200','https://ecommercev2-ytjg.onrender.com'],
     optionsSucessStatus:200,
@@ -102,16 +37,18 @@ connection.connect((err) => {
 
 // Route to fetch products from the database
 app.get('/product/items', (req, res) => {
-    connection.query('SELECT product_name, product_description, product_image, product_price, setup_type FROM products', (err, results) => {
+    connection.query(process.env.FETCH_QUERY, (err, results) => {
         if (err) {
             console.error('Error fetching products', err);
             res.status(500).json({ error: 'Failed to fetch products' });
         } else {
-            console.log('PRODUCTS::', results);
             res.json(results);
         }
     });
 });
+
+
+
 
 app.post('/form', (req, res) => {
     const data = req.body;
